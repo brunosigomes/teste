1c4fd6df5dca7492eb87ebe502e713ce272bb633
Bruno Gomes <bruno.gomes@flexup.com.br>
2020-12-10 23:06:15 -0300

    Ajuste HomeController

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -4,21 +4,13 @@ class HomeController
 {
 	public function index()
 	{
-		try {
-			// $postagens = Postagem::selecionaTodos();
+		$loader = new \Twig\Loader\FilesystemLoader('app/View');
+		$twig = new \Twig\Environment($loader);
+		$template = $twig->load('home/index.html');
 
-			$loader = new \Twig\Loader\FilesystemLoader('app/View');
-			$twig = new \Twig\Environment($loader);
-			$template = $twig->load('home/index.html');
+		$params = array();
 
-			$params = array();
-			// $params['postagens'] = $postagens;
-
-			$conteudo = $template->render($params);
-			echo $conteudo;
-			
-		} catch (Exception $e) {
-			echo $e->getMessage();
-		}
+		$conteudo = $template->render($params);
+		echo $conteudo;
 	}
 }
